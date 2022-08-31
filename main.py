bad8da2397a49c51896f26b3d5c3d668c86ab117
Royal Krrishna <91977328+RoyalKrrishna@users.noreply.github.com>
2022-08-31 13:11:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+# @RoyalKrrishna
+from Config import AUTH_CHANNEL, AUTH_USERS, CUSTOM_FILE_CAPTION, API_KEY, AUTH_GROUPS, TUTORIAL, BUTTON, START_MSG, AUTO_DEL, MOVIE_WEBSITE
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+from pyrogram import Client, filters
+import re
+import asyncio
+from pyrogram.errors import UserNotParticipant
+from LuciferMoringstar_Robot import get_filter_results, get_file_details, is_subscribed, get_poster
+from LuciferMoringstar_Robot import RATING, GENRES, HELP, ABOUT, OPEN
+import requests
+import random
+BUTTONS = {}
+BOT = {}
+import urllib.parse
+
+#  Private chat settings
 @Client.on_message(filters.text & filters.private & filters.incoming & filters.user(AUTH_USERS) if AUTH_USERS else filters.text & filters.private & filters.incoming)
 async def filter(client, message):
     if message.text.startswith("/"):
@@ -48,3 +64,401 @@ async def filter(client, message):
         search = await validate_query(search)
         search = urllib.parse.quote(search)
         btn.append([InlineKeyboardButton(text="👉 Click Here To Download 👈", url=f"https://{MOVIE_WEBSITE}/q?search={search}")])
+
+
+
+        if not btn:
+            return
+
+        if len(btn) > 10: 
+            btns = list(split_list(btn, 10)) 
+            keyword = f"{message.chat.id}-{message.message_id}"
+            BUTTONS[keyword] = {
+                "total" : len(btns),
+                "buttons" : btns
+            }
+        else:
+            buttons = btn
+            buttons.append(
+                [InlineKeyboardButton('How To Open Link', url=f'{TUTORIAL}')]
+            )
+#           buttons.append(
+#               [InlineKeyboardButton(text="📃 Pages 1/1",callback_data="pages")]
+#           )
+            if BUTTON:
+                buttons.append([InlineKeyboardButton(text="Close ❌",callback_data="close")])
+            poster=None
+            if API_KEY:
+                poster=await get_poster(search)
+            if poster:
+                proc_msg = await message.reply_photo(photo=poster, caption=mo_tech_yt, reply_markup=InlineKeyboardMarkup(buttons))
+                # await search_msg.delete()
+                await asyncio.sleep(AUTO_DEL)
+                await proc_msg.delete()
+
+            else:
+                proc_msg = await message.reply_text(mo_tech_yt, reply_markup=InlineKeyboardMarkup(buttons))
+                # await search_msg.delete()
+                await asyncio.sleep(AUTO_DEL)
+                await proc_msg.delete()
+            return
+
+        data = BUTTONS[keyword]
+        buttons = data['buttons'][0].copy()
+
+        buttons.append(
+            [InlineKeyboardButton('🔗 How To Open Link 🔗', url=f'{TUTORIAL}')]
+        )
+        buttons.append(
+            [InlineKeyboardButton(text="NEXT ⏩",callback_data=f"next_0_{keyword}")]
+        )    
+        buttons.append(
+            [InlineKeyboardButton(text=f"📃 Pages 1/{data['total']}", callback_data="pages")]
+        )
+        if BUTTON:
+            buttons.append([InlineKeyboardButton(text="Close ❌",callback_data="close")])
+        poster=None
+        if API_KEY:
+            poster=await get_poster(search)
+        if poster:
+            proc_msg = await message.reply_photo(photo=poster, caption=mo_tech_yt, reply_markup=InlineKeyboardMarkup(buttons))
+            # await search_msg.delete()
+            await asyncio.sleep(AUTO_DEL)
+            await proc_msg.delete()
+        else:
+            proc_msg = await message.reply_text(mo_tech_yt, reply_markup=InlineKeyboardMarkup(buttons))
+            # await search_msg.delete()
+            await asyncio.sleep(AUTO_DEL)
+            await proc_msg.delete()
+
+#  Group messages from_user
+@Client.on_message(filters.text & filters.group & filters.incoming & filters.chat(AUTH_GROUPS) if AUTH_GROUPS else filters.text & filters.group & filters.incoming)
+async def group(client, message):
+    if re.findall("((^\/|^,|^!|^\.|^[\U0001F600-\U000E007F]).*)", message.text):
+        return
+        
+    if AUTH_CHANNEL:
+        invite_link = await client.create_chat_invite_link(int(AUTH_CHANNEL))
+        try:
+            user = await client.get_chat_member(int(AUTH_CHANNEL), message.from_user.id)
+            if user.status == "kicked":
+                await client.send_message(
+                    chat_id=message.chat.id,
+                    text="Sorry Sir, You are Banned to use me.",
+                    parse_mode="markdown",
+                    disable_web_page_preview=True
+                )
+                return
+        except UserNotParticipant:
+            await client.send_message(
+                chat_id=message.chat.id,
+                text="**Hey! 😃\n\nYou Have To Join Our Update Channel To Use Me ✅\n\nClick Bellow Button To Join Now.👇🏻**",
+                reply_markup=InlineKeyboardMarkup(
+                    [
+                        [
+                            InlineKeyboardButton("🍿 Join Our Channel 🍿", url=invite_link.invite_link)
+                        ],
+                    ]
+                ),
+                parse_mode="markdown"
+            )
+            return
+
+    if 2 < len(message.text) < 50:    
+        btn = []
+        search = message.text
+        mo_tech_yt = f"**🗂️ Title:** {search}\n**⭐ Rating:** {random.choice(RATING)}\n**🎭 Genre:** {random.choice(GENRES)}\n**"
+
+        search = await validate_query(search)
+        search = urllib.parse.quote(search)
+        btn.append([InlineKeyboardButton(text="👉 Click Here To Download 👈", url=f"https://{MOVIE_WEBSITE}/q?search={search}")])
+
+        if not btn:
+            return
+
+        if len(btn) > 10: 
+            btns = list(split_list(btn, 10)) 
+            keyword = f"{message.chat.id}-{message.message_id}"
+            BUTTONS[keyword] = {
+                "total" : len(btns),
+                "buttons" : btns
+            }
+        else:
+            buttons = btn
+            buttons.append(
+                [InlineKeyboardButton('How To Open Link', url=f'{TUTORIAL}')]
+            )
+#           buttons.append(
+#               [InlineKeyboardButton(text="📃 Pages 1/1",callback_data="pages")]
+#           )
+            if BUTTON:
+                buttons.append([InlineKeyboardButton(text="Close ❌",callback_data="close")])
+            poster=None
+            if API_KEY:
+                poster=await get_poster(search)
+            if poster:
+                proc_msg = await message.reply_photo(photo=poster, caption=mo_tech_yt, reply_markup=InlineKeyboardMarkup(buttons))
+                # await search_msg.delete()
+                await asyncio.sleep(AUTO_DEL)
+                await proc_msg.delete()
+            else:
+                proc_msg = await message.reply_text(mo_tech_yt, reply_markup=InlineKeyboardMarkup(buttons))
+                # await search_msg.delete()
+                await asyncio.sleep(AUTO_DEL)
+                await proc_msg.delete()
+            return
+
+        data = BUTTONS[keyword]
+        buttons = data['buttons'][0].copy()
+
+        buttons.append(
+            [InlineKeyboardButton('🔗 How To Open Link 🔗', url=f'{TUTORIAL}')]
+        )
+        buttons.append(
+            [InlineKeyboardButton(text="NEXT ⏩",callback_data=f"next_0_{keyword}")]
+        )    
+        buttons.append(
+            [InlineKeyboardButton(text=f"📃 Pages 1/{data['total']}", callback_data="pages")]
+        )
+        if BUTTON:
+            buttons.append([InlineKeyboardButton(text="Close ❌",callback_data="close")])
+        poster=None
+        if API_KEY:
+            poster=await get_poster(search)
+        if poster:
+            proc_msg = await message.reply_photo(photo=poster, caption=mo_tech_yt, reply_markup=InlineKeyboardMarkup(buttons))
+            # await search_msg.delete()
+            await asyncio.sleep(AUTO_DEL)
+            await proc_msg.delete()
+        else:
+            proc_msg = await message.reply_text(mo_tech_yt, reply_markup=InlineKeyboardMarkup(buttons))
+            # await search_msg.delete()
+            await asyncio.sleep(AUTO_DEL)
+            await proc_msg.delete()
+
+    
+def get_size(size):
+    """Get size in readable format"""
+
+    units = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB"]
+    size = float(size)
+    i = 0
+    while size >= 1024.0 and i < len(units):
+        i += 1
+        size /= 1024.0
+    return "%.2f %s" % (size, units[i])
+
+def split_list(l, n):
+    for i in range(0, len(l), n):
+        yield l[i:i + n]          
+
+
+@Client.on_callback_query()
+async def cb_handler(client: Client, query: CallbackQuery):
+    clicked = query.from_user.id
+    try:
+        typed = query.message.reply_to_message.from_user.id
+    except:
+        typed = query.from_user.id
+        pass
+    if (clicked == typed):
+
+        if query.data.startswith("next"):
+            ident, index, keyword = query.data.split("_")
+            try:
+                data = BUTTONS[keyword]
+            except KeyError:
+                await query.answer("You are using this for one of my old message, please send the request again.",show_alert=True)
+                return
+
+            if int(index) == int(data["total"]) - 2:
+                buttons = data['buttons'][int(index)+1].copy()
+
+                buttons.append(
+                    [InlineKeyboardButton('🔗 How To Open Link 🔗', url=f'{TUTORIAL}')]
+                )
+                buttons.append(
+                    [InlineKeyboardButton("⏪ BACK", callback_data=f"back_{int(index)+1}_{keyword}")]
+                )
+                buttons.append(
+                    [InlineKeyboardButton(f"📃 Pages {int(index)+2}/{data['total']} 📃", callback_data="pages")]
+                )
+                if BUTTON:
+                    buttons.append([InlineKeyboardButton(text="Close ❌",callback_data="close")])
+
+                await query.edit_message_reply_markup( 
+                    reply_markup=InlineKeyboardMarkup(buttons)
+                )
+                return
+            else:
+                buttons = data['buttons'][int(index)+1].copy()
+
+                buttons.append(
+                [InlineKeyboardButton('🔗 How To Open Link 🔗', url=f'{TUTORIAL}')]
+                )
+                buttons.append(
+                    [InlineKeyboardButton("⏪ BACK", callback_data=f"back_{int(index)+1}_{keyword}"),InlineKeyboardButton("NEXT ⏩", callback_data=f"next_{int(index)+1}_{keyword}")]
+                )
+                buttons.append(
+                    [InlineKeyboardButton(f"📃 Pages {int(index)+2}/{data['total']} 📃", callback_data="pages")]
+                )
+                if BUTTON:
+                    buttons.append([InlineKeyboardButton(text="Close ❌",callback_data="close")])
+
+                await query.edit_message_reply_markup( 
+                    reply_markup=InlineKeyboardMarkup(buttons)
+                )
+                return
+
+
+        elif query.data.startswith("back"):
+            ident, index, keyword = query.data.split("_")
+            try:
+                data = BUTTONS[keyword]
+            except KeyError:
+                await query.answer("You are using this for one of my old message, please send the request again.",show_alert=True)
+                return
+
+            if int(index) == 1:
+                buttons = data['buttons'][int(index)-1].copy()
+
+                buttons.append(
+                    [InlineKeyboardButton('🔗 How To Open Link 🔗', url=f'{TUTORIAL}')]
+                )
+                buttons.append(
+                    [InlineKeyboardButton("NEXT ⏩", callback_data=f"next_{int(index)-1}_{keyword}")]
+                )
+                buttons.append(
+                    [InlineKeyboardButton(f"📃 Pages {int(index)}/{data['total']} 📃", callback_data="pages")]
+                )
+                if BUTTON:
+                    buttons.append([InlineKeyboardButton(text="Close ❌",callback_data="close")])
+
+                await query.edit_message_reply_markup( 
+                    reply_markup=InlineKeyboardMarkup(buttons)
+                )
+                return   
+            else:
+                buttons = data['buttons'][int(index)-1].copy()
+
+                buttons.append(
+                    [InlineKeyboardButton('🔗 How To Open Link 🔗', url=f'{TUTORIAL}')]
+                )
+                buttons.append(
+                    [InlineKeyboardButton("⏪ BACK", callback_data=f"back_{int(index)-1}_{keyword}"),InlineKeyboardButton("NEXT ⏩", callback_data=f"next_{int(index)-1}_{keyword}")]
+                )
+                buttons.append(
+                    [InlineKeyboardButton(f"📃 Pages {int(index)}/{data['total']} 📃", callback_data="pages")]
+                )
+                if BUTTON:
+                    buttons.append([InlineKeyboardButton(text="Close ❌",callback_data="close")])
+
+                await query.edit_message_reply_markup( 
+                    reply_markup=InlineKeyboardMarkup(buttons)
+                )
+                return
+        elif query.data == "open":
+            buttons = [
+                [
+                    InlineKeyboardButton('🔗 How To Open Link 🔗', url=f'{TUTORIAL}')
+                ]
+                ]
+            await query.message.edit(text=f"{OPEN}", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True),
+        elif query.data == "help":
+            buttons = [
+                [
+                    InlineKeyboardButton('How To Open Link❓', url=f'{TUTORIAL}')
+                ]
+                ]
+            await query.message.edit(text=f"{HELP}", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True),
+        elif query.data == "about":
+            buttons = [
+                [
+                    InlineKeyboardButton('How To Open Link❓', url=f'{TUTORIAL}')
+                ]
+                ]
+            await query.message.edit(text=f"{ABOUT}", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
+
+        elif query.data.startswith("subinps"):
+            ident, file_id = query.data.split("#")
+            filedetails = await get_file_details(file_id)
+            for files in filedetails:
+                title = files.file_name
+                size=get_size(files.file_size)
+                f_caption=files.caption
+                if CUSTOM_FILE_CAPTION:
+                    try:
+                        f_caption=CUSTOM_FILE_CAPTION.format(file_name=title, file_size=size, file_caption=f_caption)
+                    except Exception as e:
+                        print(e)
+                        f_caption=f_caption
+                if f_caption is None:
+                    f_caption = f"{files.file_name}"
+                buttons = [
+                    [
+                        InlineKeyboardButton("💢 Our Group", url="https://t.me/iPopcornMovieGroup"),
+                        InlineKeyboardButton("Report ❗", url="https://t.me/RoyalKrrishna")
+                    ],
+                    [
+                        InlineKeyboardButton("How To Open Link❓", url=f'{TUTORIAL}')
+                    ]
+                ]
+                
+                await query.answer()
+                await client.send_cached_media(
+                    chat_id=query.from_user.id,
+                    file_id=file_id,
+                    caption=f_caption,
+                    reply_markup=InlineKeyboardMarkup(buttons)
+                    )
+        elif query.data.startswith("checksub"):
+            if AUTH_CHANNEL and not await is_subscribed(client, query):
+                await query.answer("I Like Your Smartness, But Don't Be Oversmart 😒",show_alert=True)
+                return
+            ident, file_id = query.data.split("#")
+            filedetails = await get_file_details(file_id)
+            for files in filedetails:
+                title = files.file_name
+                size=get_size(files.file_size)
+                f_caption=files.caption
+                if CUSTOM_FILE_CAPTION:
+                    try:
+                        f_caption=CUSTOM_FILE_CAPTION.format(file_name=title, file_size=size, file_caption=f_caption)
+                    except Exception as e:
+                        print(e)
+                        f_caption=f_caption
+                if f_caption is None:
+                    f_caption = f"{title}"
+                buttons = [
+                    [
+                        InlineKeyboardButton('Our Group', url='https://t.me/iPopcornMovieGroup'),
+                        InlineKeyboardButton('Tutorial', url=f'{TUTORIAL}')
+                    ]
+                    ]
+                
+                await query.answer()
+                await client.send_cached_media(
+                    chat_id=query.from_user.id,
+                    file_id=file_id,
+                    caption=f_caption,
+                    reply_markup=InlineKeyboardMarkup(buttons)
+                    )
+
+
+        elif query.data == "pages":
+            await query.answer('Click on the titles!')
+        elif query.data == "close":
+            try:
+                await query.message.reply_to_message.delete()
+                await query.message.delete()
+            except:
+                await query.message.delete()
+                
+    else:
+        await query.answer("Don't Be Angry!😚 Just Type A Name Below 👇🏻",show_alert=True)
+
+
+async def validate_query(query):
+    query = re.sub(r"\b(pl(i|e)*?(s|z+|ease|se|ese|(e+)s(e)?)|((send|snd|giv(e)?|gib)(\sme)?)|movie(s)?|new|hd|movie|hindi|dubbed|dedo|print|full|latest|mal(ayalam)?|t(h)?amil|that|kittum(o)*|full\smovie|subtitle(s)?)", "", query.lower(), flags=re.IGNORECASE)
+
+    return query
